5a4ed0383522bc3f21e57286544c873a979d4e66
Janet <jlee67@syr.edu>
2021-01-16 00:15:01 -0500

    Add files via upload

---
+++ Scientific Calculator.py
@@ -0,0 +1,10 @@
+#Create a Scientific Calculator
+
+from tkinter import *
+from math import *
+
+win = Tk()
+win.title("Scientific Calculator")
+input = Entry(win, width = 40)
+input.grid()
+win.mainloop()
