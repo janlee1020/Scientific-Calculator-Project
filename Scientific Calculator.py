31c47b90ee17d23f17a6b2beccb21da2e4c93016
Janet <jlee67@syr.edu>
2021-01-17 19:01:55 -0500

    Add files via upload

--- Scientific Calculator.py
+++ Scientific Calculator.py
@@ -7,4 +7,16 @@ win = Tk()
 win.title("Scientific Calculator")
 input = Entry(win, width = 40, fg = "White", bg = "Black")
 input.grid(column = 0, row = 0, padx = 5, pady = 5, columnspan = 5)
+
+def clear():
+    input.delete(0, END)
+
+def stringtoAnswer():
+    result = input.get()
+    result = eval(result)
+    input.delete(0, END)
+    input.insert(0, result)
+
+
+log = Button(win, )
 win.mainloop()
